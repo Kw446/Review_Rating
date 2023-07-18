af52e85b3bb035f12db9de66f897adea319816ad
kaustubh wani <kaustubhwani446@gmail.com>
2023-07-18 18:36:23 +0530

    loggeradded

--- config/modelConfig.js
+++ config/modelConfig.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 // mongoose.connect('mongodb://localhost:27017/basic1');
+const logger=require("../utils/loger");
 
 mongoose.connect(process.env.URL, {
   useNewUrlParser: "true",
 });
 mongoose.connection.on("error", (err) => {
   console.log("mongoose Connection Error", err);
+  logger.log('error',"mongoose connection error");
 });
 mongoose.connection.on("connected", (err, res) => {
   console.log("mongoose is connected");
+  logger.log('info',"mongoose connected success");
 });
   
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ require("./config/modelConfig");
 let userRouter = require("./routes/mainRoutes");
 const router = require("./routes/userRoutes");
 const companyRoutes = require("./routes/companyRoutes");
+const logger = require("./utils/loger");
+const PORT =process.env.PORT||5000;
+const HOST = "localhost";
+
 
 app.get("/send", async (req, res) => {
   transporter.sendMail(mailOption, (error, info) => {
@@ -38,4 +42,5 @@ app.use("/", companyRoutes);
 
 app.listen(process.env.PORT, (req, res) => {
   console.log(`port is running on port :${process.env.PORT}`);
+  logger.info(`Server staterd and running on http://${HOST}:${PORT}`);
 });
